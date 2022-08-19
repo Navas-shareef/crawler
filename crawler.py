57ca7fbfeeec43ca043f2bea953f8eaaabdb272f
Navas Shareef <navasshareef@hamon.in>
2022-08-19 11:56:01 +0530

    implimented crawl

--- crawler.py
+++ crawler.py
@@ -2,17 +2,19 @@ import argparse
 import logging
 import os
 
+
+from bs4 import BeautifulSoup 
 import requests
-from bs4 import BeautifulSoup
 
 logger = None
 
 def parse_args():
-    parser = argparse.ArgumentParser(description = "Web crawler")
-    parser.add_argument("-d", "--debug", help = "Enable debug logging", action="store_true")
+    parser = argparse.ArgumentParser(description = "web crawler")
+    parser.add_argument("-d", "--debug", help = "Enable debug logging", action = "store_true")
+    parser.add_argument("-download", help = "creates a directory for lyrics", action = "store")
     return parser.parse_args()
 
-def configure_logging(level=logging.INFO):
+def configure_logging(level = logging.INFO):
     global logger
     logger = logging.getLogger("crawler")
     logger.setLevel(level)
@@ -22,57 +24,42 @@ def configure_logging(level=logging.INFO):
     screen_handler.setFormatter(formatter)
     logger.addHandler(screen_handler)
 
-
-def get_artists(artists):
+def get_artists(base):
+    artists = {}
+    resp = requests.get(base)
+    soup = BeautifulSoup(resp.content, "lxml")
+    track_lists = soup.find("table", attrs = {"class" : "tracklist"})
+    track_link = track_lists.find_all('h3')
+    for link in track_link[:5]:
+        artists[link.text] = link.a['href']
+    return artists
+    
+def get_artist_songs(artists):
+    songs = {}
     resp = requests.get(artists)
     soup = BeautifulSoup(resp.content, "lxml")
-    track_list = soup.find("table", attrs = {"class" : "tracklist"})
-    track_link = track_list.find_all('td',{"class":"td-item td-last"})
-    artists={}
-    for link in range(0,5):
-            artists[track_link[link].find('a').text]=track_link[link].find('a')['href']
-    return artists
+    song_list = soup.find("table", attrs = {"class" : "tracklist"})
+    songs_links = song_list.find_all('a')
+    for song in songs_links[:3]:
+        songs[song.text] = song['href']
+    return songs
 
-def get_artists_songs(singer_name,artists_songs):
-    resp = requests.get(artists_songs)
-    soup = BeautifulSoup(resp.content, "lxml")
-    song_lists = soup.find("table", attrs = {"class" : "tracklist"})
-    songs_list = song_lists.find_all('a')
-    albums={}
-    for song in range(0,5):
-        albums[songs_list[song].text]=songs_list[song]['href']
-        # logger.info(songs_list[song].text)        
-    return albums
-
-def get_atrists_song_lyrics(song_lyrics):
+def get_artists_song_lyrics(song_lyrics):
     resp = requests.get(song_lyrics)
     soup = BeautifulSoup(resp.content, "lxml")
     lyrics = soup.find('p', attrs = {"id" : "songLyricsDiv"})
-    # lyrics=lyrics.find_all('br')
-    lines=[]
-    # print(lyrics.text)
-   
-    # for line in range(0,5):
-    #     lines.append(lyrics[line].text)
-    #     print(line.text)
+    return lyrics.text
 
-
-    
-
-
-    # for lyric_link in artist_albums:
-    #     lyrics_links.append(lyric_link.find('a')['href'])
-    #     res=requests.get(lyric_link.find('a')['href'])   
-    #     soup=BeautifulSoup(res.content)
-    #     content=soup.find('div',{"class":"lyrics-inner-col-wrap"})
-
-    #     print(content.find('p',{"class":"songLyricsV14 iComment-text"}).get_text()[:10])
-    
-
-    
-    
-       
-    logger.debug("Completed crawling")
+def crawl(download_dir):
+    for artist_name, artist_link in get_artists('http://www.songlyrics.com/top-artists-lyrics.html').items():
+        logger.debug("Artist : %s", artist_name)
+        artist_dir = os.path.join(download_dir, artist_name)
+        os.makedirs(artist_dir, exist_ok = True)
+        for song, song_link in get_artist_songs(artist_link).items():
+            song_name = song.replace("/", " ")
+            file = open(f"{artist_dir}/{song_name}.txt", "w")
+            file.write(get_artists_song_lyrics(song_link))
+            file.close()
 
 def main():
     args = parse_args()
@@ -80,28 +67,7 @@ def main():
         configure_logging(logging.DEBUG)
     else:
         configure_logging(logging.INFO)
-   
-    artists=get_artists('https://www.songlyrics.com/top-artists-lyrics.html')
-    # print(artists)
-    
+    crawl("artists")
 
-    data={}
-    for singer,songlink in artists.items():
-        artist_dir=os.path.join('/home/navas/Desktop/jul-2022-crawler',singer)
-        os.makedirs(artist_dir,exist_ok=True)
-        data[singer]=get_artists_songs(singer,artists[singer])
-
-    
-    # print(data)
-    
-    print(data.values())
-
-    for singer,songs in data.items():  
-        for song,link in data[singer].items():
-            file=open(f'/home/navas/Desktop/jul-2022-crawler/{singer}/{song}.txt','w')  
-            get_atrists_song_lyrics(link)
-    
-   
-
-if __name__ == "__main__":
+if __name__=="__main__":
     main()
